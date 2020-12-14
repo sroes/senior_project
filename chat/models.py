eda782b4d581bb9a4de852094f330e839fecaef0
Scott Roes <scottroes@Scotts-MacBook-Pro.local>
2020-12-14 15:35:03 -0800

    added post_save function

--- chat/models.py
+++ chat/models.py
@@ -1,10 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
-
-User = get_user_model()
-
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE, blank=True, null=True, default='')
@@ -20,10 +18,15 @@ class Message(models.Model):
 
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     picture = models.ImageField(upload_to='', default="defaultProfilePic.jpg", null=True, blank=True)
     #book = models.ManyToManyField(Book)
     def __str__(self):
         return self.user.username
 
+
+@receiver(post_save, sender=User)   
+def post_save_user_model_receiver(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
