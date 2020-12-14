0873efe1984dab72352e57d91691b449007cc139
Scott Roes <scottroes@Scotts-MacBook-Pro.local>
2020-12-14 15:27:54 -0800

    fixed profile issues

--- friends/models.py
+++ friends/models.py
@@ -4,12 +4,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.core.exceptions import ObjectDoesNotExist
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     slug = models.SlugField()
     friends = models.ManyToManyField("Profile", blank=True)
+    
 
     def __str__(self):
         return str(self.user.username)
@@ -18,11 +20,12 @@ class Profile(models.Model):
     	return "/users/{}".format(self.slug)
 
 
-def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
-    try:
-        instance.profile.save()
-    except ObjectDoesNotExist:
-        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)   
+def post_save_user_model_receiver(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, slug=instance.username)
+
+post_save.connect(post_save_user_model_receiver, sender=User)
 
 class FriendRequest(models.Model):
 	to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE)
